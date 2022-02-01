da87ddbd56d1d6cba188843c6fe61454ecba9ede
Roderick <enuke4@gmail.com>
2022-02-01 16:09:23 -0600

    complete exercise 5 - output matches the database

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue_all_stores = Store.sum(:annual_revenue)
+puts "Total Revenue of All Stores: #{@total_revenue_all_stores.floor}"
+
+@average_revenue_all_stores = Store.average(:annual_revenue)
+puts "Average Revenue of All Stores: #{@average_revenue_all_stores.floor}"
+
+#Store count generating more than 1M in annual sales
+@stores_count = Store.where("annual_revenue >= 1000000").count
+puts "Number of Stores Generating More Than 1M in Anuual Sales: #{@stores_count}"
