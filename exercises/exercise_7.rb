13c84d13bd72cadbe02ff14cee874c3a37450fa1
Roderick Nuque <enuke4@gmail.com>
2022-02-02 02:47:57 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please Enter a Store Name:"
+@store_name = gets.chomp
+
+# errors.full_messages returns an array of all 
+# the error messages which is why you see the 
+# brackets and quotes. You can use .to_sentence 
+# to turn that array into a readable sentence.
+@store_name = Store.create(name: @store_name)
+puts "Display Error Message: #{@store_name}.errors.full_messages.to_sentence"
+
